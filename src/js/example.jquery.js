731bcb388461c71dc8bb9e693ba2270de5d5dce5
Todd Miller <toddses@gmail.com>
2017-01-05 12:29:04 -0500

    first pass at javascript stubs

---
+++ src/js/example.jquery.js
@@ -0,0 +1,26 @@
+/**
+ * This file represents the template for building out vanilla javascript functionality. Simply
+ * copy this file and rename to whatever the module is named (keep the .jquery.js filename,
+ * so it's obvious which components are jquery based). Later it will be compiled
+ * into the core javascript file and new components do not need to be added to the .info file.
+ * ES6 syntax is available via the Babel transpiler, so feel free to use modern syntax
+ * and features.
+ *
+ * Each "component" should live in its own file. This allows for separation of concerns and
+ * ease of locating functionality, and is just generally good practice.
+ *
+ * This file will be all commented out so as not to tarnish the compiled javascript with
+ * example or template code.
+ *
+ * Each file should begin with a header stub, just like this one, with a short description
+ * of the component and an author so we know who to blame.
+ * @author Todd Miller <todd.miller@tricomb2b.com>
+ */
+
+/*
+(function ($) {
+  'use strict';
+
+  // add functionality here
+})(jQuery);
+*/
